2032280871275100e9c831cc116d5ec0943f17c2
branow <bodnarorestdraw@gmail.com>
2024-09-15 16:02:50 +0300

    debug test

--- scrape/scraper_test.go
+++ scrape/scraper_test.go
@@ -44,7 +44,7 @@ func test(t *testing.T, c ScrapeCfg) {
 	assert.Equal(t, c.exp, c.o)
 }
 
-func TestScraper_Scrape_Scrape(t *testing.T) {
+func TestScraper_Scrape(t *testing.T) {
 	htmldata := `<div class="webtop"><h1 class="headword" id="direct_h_1" opal_spoken="y" random="y" htag="h1" hclass="headword" ox3000="y" opal_written="y">direct</h1> <span class="pos" hclass="pos" htag="span">adjective</span><div class="symbols" hclass="symbols" htag="div"><a href="https://www.oxfordlearnersdictionaries.com/wordlists/oxford3000-5000?dataset=english&amp;list=ox3000&amp;level=a2"><span class="ox3ksym_a2">&nbsp;</span></a><a href="https://www.oxfordlearnersdictionaries.com/wordlists/opal?dataset=english&amp;list=opal_written&amp;level=sublist_3"><span class="opal_symbol" href="OPAL_Written::Sublist_3">OPAL W</span></a><a href="https://www.oxfordlearnersdictionaries.com/wordlists/opal?dataset=english&amp;list=opal_spoken&amp;level=sublist_4"><span class="opal_symbol" href="OPAL_Spoken::Sublist_4">OPAL S</span></a></div><span class="phonetics"> <div class="phons_br" htag="div" geo="br" wd="direct" hclass="phons_br"><div class="sound audio_play_button pron-uk icon-audio" data-src-mp3="https://www.oxfordlearnersdictionaries.com/media/english/uk_pron/d/dir/direc/direct__gb_7.mp3" data-src-ogg="https://www.oxfordlearnersdictionaries.com/media/english/uk_pron_ogg/d/dir/direc/direct__gb_7.ogg" title="direct pronunciation
                     English" style="cursor: pointer" valign="top">&nbsp;</div><span class="phon">/dəˈrekt/</span><span class="sep">,</span> <div class="sound audio_play_button pron-uk icon-audio" data-src-mp3="https://www.oxfordlearnersdictionaries.com/media/english/uk_pron/d/dir/direc/direct__gb_8.mp3" data-src-ogg="https://www.oxfordlearnersdictionaries.com/media/english/uk_pron_ogg/d/dir/direc/direct__gb_8.ogg" title="direct pronunciation
                     English" style="cursor: pointer" valign="top">&nbsp;</div><span class="phon">/daɪˈrekt/</span></div> <div class="phons_n_am" geo="n_am" htag="div" hclass="phons_n_am" wd="direct"><div class="sound audio_play_button pron-us icon-audio" data-src-mp3="https://www.oxfordlearnersdictionaries.com/media/english/us_pron/d/dir/direc/direct__us_1_rr.mp3" data-src-ogg="https://www.oxfordlearnersdictionaries.com/media/english/us_pron_ogg/d/dir/direc/direct__us_1_rr.ogg" title="direct pronunciation
@@ -178,6 +178,7 @@ func TestScraper_Scrape_ScrapeString(t *testing.T) {
 		{
 			CaseName: "not strict mod",
 			doc:      getDoc(""),
+			selector: "p",
 			o:        &s1,
 			exp:      &s2,
 		},
