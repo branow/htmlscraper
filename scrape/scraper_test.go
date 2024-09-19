4ef599581d29750b910ed2bc18218491cf7316ef
branow <bodnarorestdraw@gmail.com>
2024-09-19 14:07:48 +0300

    add scraping pointers tests

--- scrape/scraper_test.go
+++ scrape/scraper_test.go
@@ -45,7 +45,7 @@ func test(t *testing.T, c ScrapeCfg) {
 }
 
 func TestScraper_Scrape(t *testing.T) {
-	htmldata := `<div class="webtop"><h1 class="headword" id="direct_h_1" opal_spoken="y" random="y" htag="h1" hclass="headword" ox3000="y" opal_written="y">direct</h1> <span class="pos" hclass="pos" htag="span">adjective</span><div class="symbols" hclass="symbols" htag="div"><a href="https://www.oxfordlearnersdictionaries.com/wordlists/oxford3000-5000?dataset=english&amp;list=ox3000&amp;level=a2"><span class="ox3ksym_a2">&nbsp;</span></a><a href="https://www.oxfordlearnersdictionaries.com/wordlists/opal?dataset=english&amp;list=opal_written&amp;level=sublist_3"><span class="opal_symbol" href="OPAL_Written::Sublist_3">OPAL W</span></a><a href="https://www.oxfordlearnersdictionaries.com/wordlists/opal?dataset=english&amp;list=opal_spoken&amp;level=sublist_4"><span class="opal_symbol" href="OPAL_Spoken::Sublist_4">OPAL S</span></a></div><span class="phonetics"> <div class="phons_br" htag="div" geo="br" wd="direct" hclass="phons_br"><div class="sound audio_play_button pron-uk icon-audio" data-src-mp3="https://www.oxfordlearnersdictionaries.com/media/english/uk_pron/d/dir/direc/direct__gb_7.mp3" data-src-ogg="https://www.oxfordlearnersdictionaries.com/media/english/uk_pron_ogg/d/dir/direc/direct__gb_7.ogg" title="direct pronunciation
+	htmldata := `<div class="webtop"><h1 class="headword" id="direct_h_1" opal_spoken="y" random="y" htag="h1" hclass="headword" ox3000="y" opal_written="y">direct</h1> <span class="pos" hclass="pos" htag="span">adjective</span><div class="symbols" hclass="symbols" htag="div"><a href="https://www.oxfordlearnersdictionaries.com/wordlists/oxford3000-5000?dataset=english;list=ox3000&amp;level=a2"><span class="ox3ksym_a2">&nbsp;</span></a><a href="https://www.oxfordlearnersdictionaries.com/wordlists/opal?dataset=english&amp;list=opal_written&amp;level=sublist_3"><span class="opal_symbol" href="OPAL_Written::Sublist_3">OPAL W</span></a><a href="https://www.oxfordlearnersdictionaries.com/wordlists/opal?dataset=english&amp;list=opal_spoken&amp;level=sublist_4"><span class="opal_symbol" href="OPAL_Spoken::Sublist_4">OPAL S</span></a></div><span class="phonetics"> <div class="phons_br" htag="div" geo="br" wd="direct" hclass="phons_br"><div class="sound audio_play_button pron-uk icon-audio" data-src-mp3="https://www.oxfordlearnersdictionaries.com/media/english/uk_pron/d/dir/direc/direct__gb_7.mp3" data-src-ogg="https://www.oxfordlearnersdictionaries.com/media/english/uk_pron_ogg/d/dir/direc/direct__gb_7.ogg" title="direct pronunciation
                     English" style="cursor: pointer" valign="top">&nbsp;</div><span class="phon">/dəˈrekt/</span><span class="sep">,</span> <div class="sound audio_play_button pron-uk icon-audio" data-src-mp3="https://www.oxfordlearnersdictionaries.com/media/english/uk_pron/d/dir/direc/direct__gb_8.mp3" data-src-ogg="https://www.oxfordlearnersdictionaries.com/media/english/uk_pron_ogg/d/dir/direc/direct__gb_8.ogg" title="direct pronunciation
                     English" style="cursor: pointer" valign="top">&nbsp;</div><span class="phon">/daɪˈrekt/</span></div> <div class="phons_n_am" geo="n_am" htag="div" hclass="phons_n_am" wd="direct"><div class="sound audio_play_button pron-us icon-audio" data-src-mp3="https://www.oxfordlearnersdictionaries.com/media/english/us_pron/d/dir/direc/direct__us_1_rr.mp3" data-src-ogg="https://www.oxfordlearnersdictionaries.com/media/english/us_pron_ogg/d/dir/direc/direct__us_1_rr.ogg" title="direct pronunciation
                     American" style="cursor: pointer" valign="top">&nbsp;</div><span class="phon">/dəˈrekt/</span><span class="sep">,</span> <div class="sound audio_play_button pron-us icon-audio" data-src-mp3="https://www.oxfordlearnersdictionaries.com/media/english/us_pron/d/dir/direc/direct__us_2_rr.mp3" data-src-ogg="https://www.oxfordlearnersdictionaries.com/media/english/us_pron_ogg/d/dir/direc/direct__us_2_rr.ogg" title="direct pronunciation
@@ -66,6 +66,11 @@ func TestScraper_Scrape(t *testing.T) {
 		return "", nil
 	}
 
+	type Symbols struct {
+		Href  string `extract:"@href"`
+		Level string `extract:"*level"`
+	}
+
 	type Phonetic struct {
 		Audio      []string `select:".sound" extract:"@data-src-mp3"`
 		Transcript []string `select:".phon" extract:"text"`
@@ -79,7 +84,7 @@ func TestScraper_Scrape(t *testing.T) {
 	type Top struct {
 		Term      string    `select:".headword" extract:"text"`
 		Pos       string    `select:".pos" extract:"text"`
-		Level     string    `select:".symbols a" extract:"*level"`
+		Symbols   *Symbols  `select:".symbols a"`
 		Phonetics Phonetics `select:".phonetics"`
 	}
 
@@ -90,9 +95,9 @@ func TestScraper_Scrape(t *testing.T) {
 			doc:        getDoc(htmldata),
 			o:          &Top{},
 			exp: &Top{
-				Term:  "direct",
-				Pos:   "adjective",
-				Level: "a2",
+				Term:    "direct",
+				Pos:     "adjective",
+				Symbols: &Symbols{Href: "https://www.oxfordlearnersdictionaries.com/wordlists/oxford3000-5000?dataset=english;list=ox3000&level=a2", Level: "a2"},
 				Phonetics: Phonetics{
 					UK: Phonetic{
 						Audio:      []string{"https://www.oxfordlearnersdictionaries.com/media/english/uk_pron/d/dir/direc/direct__gb_7.mp3", "https://www.oxfordlearnersdictionaries.com/media/english/uk_pron/d/dir/direc/direct__gb_8.mp3"},
@@ -104,6 +109,36 @@ func TestScraper_Scrape(t *testing.T) {
 					},
 				},
 			},
+		},
+	}
+	tab.RunWithCfgs(t, cfgs, test)
+}
+
+func TestScraper_Scrape_ScrapePointer(t *testing.T) {
+	s1, s2 := "top", "golang"
+	type B struct {
+		KeyB *string `extract:"text"`
+	}
+	type A struct {
+		KeyA *string `extract:"@id"`
+		B    *B      `select:".con"`
+	}
+	cfgs := []ScrapeCfg{
+		{
+			CaseName: "pointers",
+			doc:      getDoc(`<div id="top"><div class="con">golang</div><div class="noc"></div></div>`),
+			o:        &A{},
+			selector: "#top",
+			extract:  "",
+			exp:      &A{KeyA: &s1, B: &B{KeyB: &s2}},
+		},
+		{
+			CaseName: "nil pointers",
+			doc:      getDoc(`<div id="top"></div>`),
+			o:        &A{},
+			selector: "#top",
+			extract:  "",
+			exp:      &A{KeyA: &s1, B: nil},
 		},
 	}
 	tab.RunWithCfgs(t, cfgs, test)
